e319ce2a9d9e4238e77bd8291fd1022c15a8bdd0
Hossam Bassem <hosam9210basem@gmail.com>
2022-01-03 07:33:53 +0200

    test:

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ExploreScreen from './ExploreScreen/ExploreScreen'
 import StockDetails from './StockDetails/StockDetails'
 function App() {

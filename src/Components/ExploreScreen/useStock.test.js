37fd1cf94efe70a4f53e481475578f755f926bc1
Hossam Bassem <hosam9210basem@gmail.com>
2022-01-04 04:01:28 +0200

    test: write tests to check if the search feature is working properly

--- src/Components/ExploreScreen/useStock.test.js
+++ src/Components/ExploreScreen/useStock.test.js
@@ -25,6 +25,30 @@ describe("useStock functions",()=>{
         expect(result.current.stocks.length).toBeGreaterThan(intitialDataLimit)
     })
 
+         it("get initial search query",async()=>{
+        const {result}= renderHook(useStock)
+        const searchKey="A"       
+
+     await act(async()=>{
+         result.current.searchIsActive(true)
+          await  result.current.getSearchResults(searchKey)
+
+        })
+        expect(result.current.stocks.length).toEqual(intitialDataLimit)
+    })
+     it("get next search stock data",async()=>{
+        const {result}= renderHook(useStock)
+               
+
+     await act(async()=>{
+          await  result.current.getNextData()
+
+        })
+        expect(result.current.stocks.length).toBeGreaterThan(intitialDataLimit)
+    })
+    
+
+
 })
 
  
